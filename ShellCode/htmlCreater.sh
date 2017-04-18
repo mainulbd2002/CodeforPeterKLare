d87701021d4d02779d180fd58e80669f98053b35
Mainul Islam <mainulbd2002@msn.com>
2017-04-18 21:20:11 +1000

    1st Success

--- ShellCode/htmlCreater.sh
+++ ShellCode/htmlCreater.sh
@@ -1,33 +1,30 @@
-#Creating script to replace HTML placeholders
+#/bin/bash
 
+input_file=$1
+prop_file=$2
+output_file=$3
 
-in_file=$1
-prop=$2
-out_file=$3
-
-if ! [ -f "$in_file" ]
-then
-	echo "Cannot find input";
-	exit 1;
+if ! [ -f "$input_file" ] 
+then 
+  echo "Input file not found.";
+  exit 1;
 fi
 
-
-if ! [ -f "$prop" ]
-then
-	echo "Cannot find properties file";
-	exit 1;
+if ! [ -f "$prop_file" ] 
+then 
+  echo "Property file not found.";
+  exit 1;
 fi
 
-if [ -z "out_file" ]
+if [ -z "$output_file" ]
 then
-	echo "Out file is not specified;
-	exit 1;
+  echo "No output file path supplied."
+  exit 1;
 fi
 
-direcotry=$(dirname "$out_file")
-mkdir -p -- "$directory"
+dir=$(dirname "$output_file")
+mkdir -p -- "$dir"
 
-sed -f <(sed '/=/ s/\([^=]*\)=\(.*\)/s#\\\[\\\[\1\\\]\\\]#\2#g/' "$prop") "${in_file}" > ${out_file}
-
-	
-
+sed -f <(
+   sed '/=/ s/\([^=]*\)=\(.*\)/s#\\\[\\\[\1\\\]\\\]#\2#g/' "$prop_file"
+        ) "${input_file}" > ${output_file}
