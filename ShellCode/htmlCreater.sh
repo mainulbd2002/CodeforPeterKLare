b382ef50723166fc6fdbea80b674055457fa73b5
Mainul Islam <mainulbd2002@msn.com>
2017-04-18 20:42:28 +1000

    initial script

--- ShellCode/htmlCreater.sh
+++ ShellCode/htmlCreater.sh
@@ -5,8 +5,29 @@ prop=$1
 in_file=$2
 out_file=$3
 
-echo $prop
-echo $in_file
-echo #out_file
+if ! [ -f "$in_file" ]
+then
+	echo "Cannot find input";
+	exit 1;
+fi
 
+
+if ! [ -f "$prop" ]
+then
+	echo "Cannot find properties file";
+	exit 1;
+fi
+
+if [ -z "out_file" ]
+then
+	echo "Out file is not specified;
+	exit 1;
+fi
+
+direcotry=$(dirname "$out_file")
+mkdir -p -- "$directory"
+
+sed -f <(sed '/=/ s/\([^=]*\)=\(.*\)/s#\\\[\\\[\1\\\]\\\]#\2#g/' "$prop") "${in_file}" > ${out_file}
+
+	
 
