cc4fb418333152154ff4db1d74dfae454ea9bd75
Mainul Islam <mainulbd2002@msn.com>
2017-04-18 20:01:18 +1000

    Completed Controller Code

--- optusCodetest/src/main/java/com/mainul/optuscodetest/SubscriptionController.java
+++ optusCodetest/src/main/java/com/mainul/optuscodetest/SubscriptionController.java
@@ -5,7 +5,14 @@
  */
 package com.mainul.optuscodetest;
 
+import com.mainul.optuscodetest.stringmanipulator.IStringTrunctor;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
 
 /**
  *
@@ -15,7 +22,29 @@ import org.springframework.stereotype.Controller;
 public class SubscriptionController {
     
     private static final String FILLER_STR = " ... (truncated) ... ";
+    @Autowired
+    IStringTrunctor strTrunctor;
     
+    @RequestMapping(value="/subscribe", method=RequestMethod.GET)
+    public String subscribe(HttpServletRequest request){
+        
+        String orderDetails = getOrderDetails(request);
+        String truncateOrderDetails = truncate(orderDetails);
+        log(truncateOrderDetails);
+        
+        return "subscription_view";
+    }
+
+    private void log(String truncateOrderDetails) {
+           // Complete logging through file 
+    }
+
+    public String truncate(String orderDetails) {
+        return strTrunctor.shortenStringWithMark(orderDetails, FILLER_STR, 30);
+    }
     
+    private String getOrderDetails(HttpServletRequest request){
+        return "76543213456789098765432134567898765432145678987654321345437876";
+    }
     
 }
